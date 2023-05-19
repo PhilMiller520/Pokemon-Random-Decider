d3e928914f8b53a682f29f7481add2d946017685
PhilMiller520 <123090492+PhilMiller520@users.noreply.github.com>
2023-05-19 17:35:33 -0600

    updated so this one imports the other files
    updated so this one imports the other files when the solo and ironmon buttons are pressed.

---
+++ Poke_Games_List.py
@@ -0,0 +1,3 @@
+# lists pokemon games gens 1 through 4
+Poke_Games = ['Red', 'Blue', 'Yellow', 'Gold', 'Silver', 'Crystal', 'Ruby', 'Sapphire', 'Emerald',
+              'Fire Red', 'Leaf Green', 'Diamond', 'Pearl', 'Platinum', 'Heart Gold', 'Soul Silver']
--- temp_poke_choser.py
+++ temp_poke_choser.py
@@ -1,72 +1,13 @@
 import tkinter as tk
 import random
+import Gen1_List #program that lists all the pokemon from gen 1
+import Gen2_List #program that lists all the pokemon added to the global dex from gen 2
+import Gen3_List #program that lists all the pokemon added to the global dex from gen 3
+import Gen4_List #program that lists all the pokemon added to the global dex from gen 4
+import Poke_Games_List #lists all games from gen1 through gen 4
 
 #lists pokeball locations for starters
 StarterLocation = ['Left', 'Middle', 'Right'] 
-
-#lists all games from gen1 through gen 4
-Poke_Games = ['Red', 'Blue', 'Yellow', 'Gold', 'Silver', 'Cyrstal', 'Ruby', 'Sapphire', 'Emerald', 
-        'Fire Red', 'Leaf Green', 'Diamond', 'Pearl', 'Platinum', 'Heart Gold', 'Soul Silver'] 
-
-#lits all pokemon in gen 1
-Gen1List = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard','Squirtle', 'Wartortle', 
-'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle','Kakuna', 'Beedrill','Pidgey','Pidgeotto', 'Pidgeot', 
-'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash',
-'Nidoran female', 'Nidorina', 'Nidoqueen', 'Nidoran male', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 
-'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 
-'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape',
-'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke',
-'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem',
-'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', 'Farfetchd', 'Doduo', 'Dodrio', 'Seel', 
-'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 
-'Krabby','Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan',
-'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra',
-'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 
-'Tauros','Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 
-'Omanyte', 'Omastar','Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 
-'Dratini', 'Dragonair', 'Dragonite','Mewtwo', 'Mew'] 
-
-#lits new pokemon introduced in gen 2
-Gen2List = ['Chikorita', 'Bayleef', 'Meganium', 'Cyndaquil', 'Quilava', 'Typhlosion', 'Totodile', 'Croconaw',
-'Feraligatr', 'Sentret', 'Furret', 'Hoothoot', 'Noctowl', 'Ledyba', 'Ledian', 'Spinarak', 'Ariados', 'Crobat',
-'Chinchou', 'Lanturn', 'Pichu', 'Cleffa', 'Igglybuff', 'Togepi', 'Togetic', 'Natu', 'Xatu', 'Mareep', 'Flaaffy',
-'Ampharos', 'Bellossom', 'Marill', 'Azumarill', 'Sudowoodo', 'Politoed', 'Hoppip', 'Skiploom', 'Jumpluff', 'Aipom',
-'Sunkern', 'Sunflora', 'Yanma', 'Wooper', 'Quagsire', 'Espeon', 'Umbreon', 'Murkrow', 'Slowking', 'Misdreavus',
-'Unown', 'Wobbuffet', 'Girafarig', 'Pineco', 'Forretress', 'Dunsparce', 'Gligar', 'Steelix', 'Snubbull', 'Granbull',
-'Qwilfish', 'Scizor', 'Shuckle', 'Heracross', 'Sneasel', 'Teddiursa', 'Ursaring', 'Slugma', 'Magcargo', 'Swinub',
-'Piloswine', 'Corsola', 'Remoraid', 'Octillery', 'Delibird', 'Mantine', 'Skarmory', 'Houndour', 'Houndoom', 'Kingdra', 
-'Phanpy', 'Donphan', 'Porygon2', 'Stantler', 'Smeargle', 'Tyrogue', 'Hitmontop', 'Smoochum', 'Elekid', 'Magby',
-'Miltank', 'Blissey', 'Raikou', 'Entei', 'Suicune', 'Larvitar', 'Pupitar', 'Tyranitar', 'Lugia', 'Ho-Oh', 'Celebi'] 
-
-#lits new pokemon introduced in gen 3
-Gen3List = ['Treecko', 'Grovyle', 'Sceptile', 'Torchic', 'Combusken', 'Blaziken', 'Mudkip', 'Marshtomp', 'Swampert', 
-'Poochyena','Mightyena', 'Zigzagoon', 'Linoone', 'Wurmple', 'Silcoon', 'Beautifly', 'Cascoon', 'Dustox', 'Lotad', 
-'Lombre', 'Ludicolo', 'Seedot', 'Nuzleaf', 'Shiftry', 'Taillow', 'Swellow', 'Wingull', 'Pelipper', 'Ralts', 'Kirlia',
-'Gardevoir', 'Surskit', 'Masquerain', 'Shroomish', 'Breloom', 'Slakoth', 'Vigoroth', 'Slaking', 'Nincada', 'Ninjask',
-'Shedinja', 'Whismur', 'Loudred', 'Exploud', 'Makuhita', 'Hariyama', 'Azurill', 'Nosepass', 'Skitty', 'Delcatty',
-'Sableye', 'Mawile', 'Aron', 'Lairon', 'Aggron', 'Meditite', 'Medicham', 'Electrike', 'Manectric', 'Plusle', 'Minun',
-'Volbeat', 'Illumise', 'Roselia', 'Gulpin', 'Swalot', 'Carvanha', 'Sharpedo', 'Wailmer', 'Wailord', 'Numel', 
-'Camerupt', 'Torkoal', 'Spoink', 'Grumpig', 'Spinda', 'Trapinch', 'Vibrava', 'Flygon', 'Cacnea', 'Cacturne', 'Swablu',
-'Altaria', 'Zangoose', 'Seviper', 'Lunatone', 'Solrock', 'Barboach', 'Whiscash', 'Corphish', 'Crawdaunt', 'Baltoy',
-'Claydol', 'Lileep', 'Cradily', 'Anorith', 'Armaldo', 'Feebas', 'Milotic', 'Castform', 'Kecleon', 'Shuppet',
-'Banette', 'Duskull', 'Dusclops', 'Tropius', 'Chimecho', 'Absol', 'Wynaut', 'Snorunt', 'Glalie', 'Spheal', 'Sealeo',
-'Walrein', 'Clamperl', 'Huntail', 'Gorebyss', 'Relicanth', 'Luvdisc', 'Bagon', 'Shelgon', 'Salamence', 'Beldum',
-'Metang', 'Metagross', 'Regirock', 'Regice', 'Registeel', 'Latias', 'Latios', 'Kyogre', 'Groudon', 'Rayquaza', 
-'Jirachi', 'Deoxys'] 
-
-#lits new pokemon introduced in gen 4
-Gen4List = ['Turtwig', 'Grotle', 'Torterra', 'Chimchar', 'Monferno', 'Infernape', 'Piplup', 'Prinplup',
-'Empoleon', 'Starly', 'Staravia', 'Staraptor', 'Bidoof', 'Bibarel', 'Kricketot', 'Kricketune', 'Shinx', 'Luxio',
-'Luxray', 'Budew', 'Roserade', 'Cranidos', 'Rampardos', 'Shieldon', 'Bastiodon', 'Burmy', 'Wormadam', 'Mothim',
-'Combee', 'Vespiquen', 'Pachirisu', 'Buizel', 'Floatzel', 'Cherubi', 'Cherrim', 'Shellos', 'Gastrodon', 'Ambipom', 
-'Drifloon', 'Drifblim', 'Buneary', 'Lopunny', 'Mismagius', 'Honchkrow', 'Glameow', 'Purugly', 'Chingling', 'Stunky',
-'Skuntank', 'Bronzor', 'Bronzong', 'Bonsly', 'Mime Jr.', 'Happiny', 'Chatot', 'Spiritomb', 'Gible', 'Gabite', 
-'Garchomp', 'Munchlax', 'Riolu', 'Lucario', 'Hippopotas', 'Hippowdon', 'Skorupi', 'Drapion', 'Croagunk', 
-'Toxicroak', 'Carnivine', 'Finneon', 'Lumineon', 'Mantyke', 'Snover', 'Abomasnow', 'Weavile', 'Magnezone', 
-'Lickilicky', 'Rhyperior', 'Tangrowth', 'Electivire', 'Magmortar', 'Togekiss', 'Yanmega', 'Leafeon', 'Glaceon', 
-'Gliscor', 'Mamoswine', 'Porygon-Z', 'Gallade', 'Probopass', 'Dusknoir', 'Froslass', 'Rotom', 'Uxie', 'Mesprit',
-'Azelf', 'Dialga', 'Palkia', 'Heatran', 'Regigigas', 'Giratina', 'Cresselia', 'Phione', 'Manaphy', 'Darkrai',
-'Shaymin', 'Arceus']
 
 #clears the message
 def clear_message():
@@ -76,31 +17,36 @@ def clear_message():
 def Solo_Challenge():
     clear_message()
     #chooses a pokemon game at random
-    PickedGame = (random.choice(Poke_Games))
+    PickedGame = (random.choice(Poke_Games_List.Poke_Games))
     
     Solo_message_label.place(x=230, y=250)
     if PickedGame in ['Red', 'Blue', 'Yellow']:   
            Solo_message_label.config(text=f"""The chosen game is {PickedGame}\n
+                " "
               The chosen pokemon for this run is: """ 
-              + random.choice(Gen1List))
-    elif PickedGame in ['Gold', 'Silver', 'Cyrstal']:  
+              + random.choice(Gen1_List.Gen1List))
+    elif PickedGame in ['Gold', 'Silver', 'Crystal']:  
            Solo_message_label.config(text=f"""The chosen game is {PickedGame}\n
+                " "
               The chosen pokemon for this run is: """ 
-              + random.choice(Gen1List + Gen2List))
+              + random.choice(Gen1_List.Gen1List + Gen2_List.Gen2List))
     elif PickedGame in ['Ruby', 'Sapphire', 'Emerald', 'Fire Red', 'Leaf Green']:  
            Solo_message_label.config(text=f"""The chosen game is {PickedGame}\n
+                " "
               The chosen pokemon for this run is: """ 
-              + random.choice(Gen1List + Gen2List + Gen3List))
+              + random.choice(Gen1_List.Gen1List + Gen2_List.Gen2List + Gen3_List.Gen3List))
     elif PickedGame in ['Diamond', 'Pearl', 'Platinum', 'Heart Gold', 'Soul Silver']:  
            Solo_message_label.config(text=f"""The chosen game is {PickedGame}\n
+                " "
               The chosen pokemon for this run is: """ 
-              + random.choice(Gen1List + Gen2List + Gen3List+ Gen4List))
+              + random.choice(Gen1_List.Gen1List + Gen2_List.Gen2List + Gen3_List.Gen3List + 
+                  Gen4_List.Gen4List))
     else: 
         Solo_message_label.config(text="selection not valid")
    
 def IronMon_Challenge():
     clear_message()
-    PickedGame = (random.choice(Poke_Games))
+    PickedGame = (random.choice(Poke_Games_List.Poke_Games))
     Pokeball_Starter =(random.choice(StarterLocation))
 
     IronMon_Challenge_label.place(x=490, y=250)
@@ -108,25 +54,28 @@ def IronMon_Challenge():
            IronMon_Challenge_label.config(text=f"""The chosen game is {PickedGame}\n
               The chosen starter is in the {Pokeball_Starter} Pokeball\n
               The Alternate Starter pokemon options are:\n""" 
-              + ", ".join(random.sample(Gen1List, k=3))) 
-    elif PickedGame in ['Gold', 'Silver', 'Cyrstal']:
+              + ", ".join(random.sample(Gen1_List.Gen1List, k=3))) 
+    elif PickedGame in ['Gold', 'Silver', 'Crystal']:
            IronMon_Challenge_label.config(text=f"""The chosen game is {PickedGame}\n
               Your starter is in the {Pokeball_Starter} Pokeball\n
               The Alternate Starter pokemon options are:\n""" 
-              + ", ".join(random.sample(Gen1List + Gen2List, k=3))) 
+              + ", ".join(random.sample(Gen1_List.Gen1List + Gen2_List.Gen2List, k=3))) 
     elif PickedGame in ['Ruby', 'Sapphire', 'Emerald', 'Fire Red', 'Leaf Green']:  
            IronMon_Challenge_label.config(text=f"""The chosen game is {PickedGame}\n
               Your starter is in the {Pokeball_Starter} Pokeball\n
               The Alternate Starter pokemon options are:\n""" 
-              + ", ".join(random.sample(Gen1List + Gen2List + Gen3List, k=3)))
+              + ", ".join(random.sample(Gen1_List.Gen1List + Gen2_List.Gen2List + 
+                        Gen3_List.Gen3List, k=3)))
     elif PickedGame in ['Diamond', 'Pearl', 'Platinum', 'Heart Gold', 'Soul Silver']:  
            IronMon_Challenge_label.config(text=f"""The chosen game is {PickedGame}\n
               Your starter is in the {Pokeball_Starter} Pokeball\n
               The Alternate Starter pokemon options are:\n""" 
-              + ", ".join(random.sample(Gen1List + Gen2List + Gen3List+ Gen4List, k=4)))
+               + ", ".join(random.sample(Gen1_List.Gen1List + Gen2_List.Gen2List + 
+                        Gen3_List.Gen3List + Gen4_List.Gen4List, k=4)))
     else: 
         IronMon_Challenge_label.config(text="""The chosen game is Yellow\n
-         The Alternate Starter pokemon options are:\n""" + ", ".join(random.sample(Gen1List, k=3))) 
+         The Alternate Starter pokemon options are:\n""" + 
+            ", ".join(random.sample(Gen1_List.Gen1List, k=3))) 
 
 def toggle_text():
       if var.get():
