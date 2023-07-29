eae302e00fe6a63801347b8f5a97e611ed1c954b
PhilMiller520 <123090492+PhilMiller520@users.noreply.github.com>
2023-07-29 16:56:11 -0600

    Add files via upload

---
+++ Box_Art_images.py
@@ -0,0 +1,20 @@
+# Box_Art_images.py
+
+box_art = {
+    'Red': 'Pokemon_Game_Pics/Red.png',
+    'Blue': 'Pokemon_Game_Pics/Blue.png',
+    'Yellow': 'Pokemon_Game_Pics/Yellow.png',
+    'Gold': 'Pokemon_Game_Pics/Gold.png',
+    'Silver': 'Pokemon_Game_Pics/Silver.png',
+    'Crystal': 'Pokemon_Game_Pics/Crystal.png',
+    'Ruby': 'Pokemon_Game_Pics/Ruby.png', 
+    'Sapphire': 'Pokemon_Game_Pics/Sapphire.png', 
+    'Emerald': 'Pokemon_Game_Pics/Emerald.png',
+    'Fire Red': 'Pokemon_Game_Pics/Fire_Red.png',
+    'Leaf Green': 'Pokemon_Game_Pics/Leaf_Green.png', 
+    'Diamond': 'Pokemon_Game_Pics/Diamond.png', 
+    'Pearl': 'Pokemon_Game_Pics/Pearl.png', 
+    'Platinum': 'Pokemon_Game_Pics/Platinum.png', 
+    'Heart Gold': 'Pokemon_Game_Pics/Heart_Gold.png', 
+    'Soul Silver': 'Pokemon_Game_Pics/Soul_Silver.png'    
+}
